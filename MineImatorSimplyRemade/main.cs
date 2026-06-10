94fe9960a1e777d2669cc1ad919c3114b71a4309
Forest <wilsonforest99@gmail.com>
2026-06-10 05:07:38 -0400

    b

--- MineImatorSimplyRemade/main.cs
+++ MineImatorSimplyRemade/main.cs
@@ -1,1 +1,55 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Drawing;
+using Silk.NET.GLFW;
+using Silk.NET.OpenGL;
+
+
+class main
+{
+    private static Glfw _glfw;
+    private static unsafe WindowHandle* _window;
+    private static GL _gl;
+    
+    private static unsafe int Main(string[] args)
+    {
+        _glfw = Glfw.GetApi();
+        if (!_glfw.Init())
+        {
+            Console.WriteLine("Failed to initialize GLFW");
+            return 1;
+        }
+        
+        _glfw.WindowHint(WindowHintInt.ContextVersionMajor, 3);
+        _glfw.WindowHint(WindowHintInt.ContextVersionMinor, 3);
+        _glfw.WindowHint(WindowHintOpenGlProfile.OpenGlProfile, OpenGlProfile.Core);
+        
+        _window = _glfw.CreateWindow(640, 480, "Mine Imator Simply Remade: Nuxi", null, null);
+        if (_window == null)
+        {
+            Console.WriteLine("Failed to create window");
+            _glfw.Terminate();
+            return 1;
+        }
+        
+        _glfw.MakeContextCurrent(_window);
+        
+        _gl = GL.GetApi(_glfw.GetProcAddress);
+        
+        _gl.ClearColor(Color.Black);
+
+        while (!_glfw.WindowShouldClose(_window))
+        {
+            _glfw.PollEvents();
+            
+            _gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            
+            _glfw.SwapBuffers(_window);
+        }
+        
+        _gl.Dispose();
+        _glfw.DestroyWindow(_window);
+        _glfw.MakeContextCurrent(null);
+        _glfw.Terminate();
+        
+        return 0;
+    }
+}
